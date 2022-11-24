dfc667c176c39f0670645e4fbb4a032e94b21318
a.dergachev <bestacc@mail.ru>
2022-11-24 12:12:51 +0300

    added solution HW - lesson_1- TaskThree (remove element from array)

---
+++ src/Lesson_1/TaskThree.java
@@ -0,0 +1,14 @@
+package Lesson_1;
+
+public class TaskThree {
+    public int removeElement(int[] nums, int val) {
+        int k = 0;
+        for (int i = 0; i <= nums.length - 1; i++) {
+            if (nums[i] != val) {
+                nums[k] = nums[i];
+                k++;
+            }
+        }
+        return k;
+    }
+}
