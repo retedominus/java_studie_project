356b58ca3719286a6e8bac9abeb856af5cc82129
a.dergachev <bestacc@mail.ru>
2022-11-26 10:16:11 +0300

    edit solution HW - lesson_1- TaskThree (remove element)

--- src/Lesson_1/TaskThree.java
+++ src/Lesson_1/TaskThree.java
@@ -5,8 +5,7 @@ public class TaskThree {
         int k = 0;
         for (int i = 0; i <= nums.length - 1; i++) {
             if (nums[i] != val) {
-                nums[k] = nums[i];
-                k++;
+                nums[k++] = nums[i];
             }
         }
         return k;
