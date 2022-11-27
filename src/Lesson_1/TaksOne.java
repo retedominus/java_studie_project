bf8a12a20cf20893cf310fe154c62a0759af3f80
a.dergachev <bestacc@mail.ru>
2022-11-27 17:09:58 +0300

    refactored code TaskThree (remove element)

--- src/Lesson_1/TaksOne.java
+++ src/Lesson_1/TaksOne.java
@@ -1,28 +1,27 @@
 package Lesson_1;
 
 public class TaksOne {
-    public void merge(int[] nums1, int m, int[] nums2, int n) {
+    public static void merge(int[] nums1, int m, int[] nums2, int n) {
         int insert = m + n - 1;
-        int si = n - 1;
-        int fi = m - 1;
+        n = n - 1;
+        m = m - 1;
 
-        while (fi >= 0 && si >= 0) {
-            if (nums1[fi] > nums2[si]) {
-                nums1[insert] = nums1[fi];
-                fi--;
+        while (m >= 0 && n >= 0) {
+            if (nums1[m] > nums2[n]) {
+                nums1[insert] = nums1[m];
+                m--;
             } else {
-                nums1[insert] = nums2[si];
-                si--;
+                nums1[insert] = nums2[n];
+                n--;
             }
             insert--;
         }
-        while (fi >= 0) {
-            nums1[insert--] = nums1[fi--];
+        while (m >= 0) {
+            nums1[insert--] = nums1[m--];
         }
-        while (si >= 0) {
-            nums1[insert--] = nums2[si--];
+        while (n >= 0) {
+            nums1[insert--] = nums2[n--];
         }
-        return;
     }
 };
 
