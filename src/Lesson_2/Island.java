7f80e448e958d9d7ea1c8ba869e66ad4ea1156ce
a.dergachev <bestacc@mail.ru>
2022-11-30 20:29:28 +0300

    modified MaxAreaOfIsland program

--- src/Lesson_2/Island.java
+++ src/Lesson_2/Island.java
@@ -1,32 +1,32 @@
 package Lesson_2;
 
 public class Island {
+    static int area;
     public static int maxAreaOfIsland(int[][] grid) {
         if (grid.length == 0 || grid[0].length == 0) return 0;
         int maxArea = 0;
-        int[] area = new int[1];
         for (int i = 0; i < grid.length; i++) {
             for (int j = 0; j < grid[0].length; j++) {
                 if (grid[i][j] == 1) {
-                    area[0] = 0;
-                    dfs(grid, i, j, area);
-                    maxArea = Math.max(area[0], maxArea);
+                    area = 0;
+                    dfs(grid, i, j);
+                    maxArea = Math.max(area, maxArea);
                 }
             }
         }
         return maxArea;
     }
 
-    private static void dfs(int[][] grid, int i, int j, int[] area) {
+    private static void dfs(int[][] grid, int i, int j) {
         if (i < 0 || i >= grid.length || j < 0 || j >= grid[0].length || grid[i][j] != 1) {
             return;
         }
         grid[i][j] = -1;
-        area[0]++;
-        dfs(grid, i + 1, j, area);
-        dfs(grid, i - 1, j, area);
-        dfs(grid, i, j + 1, area);
-        dfs(grid, i, j - 1, area);
+        area++;
+        dfs(grid, i + 1, j);
+        dfs(grid, i - 1, j);
+        dfs(grid, i, j + 1);
+        dfs(grid, i, j - 1);
     }
 
     public static void main(String[] args) {
